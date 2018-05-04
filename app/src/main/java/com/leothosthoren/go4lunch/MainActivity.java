446567908ad7a5cf21cb436a678f1e4ee6acb77e
Leothos_Thoren <soussou.med@live.fr>
2018-05-04 21:22:00 +0200

    Update background image

--- app/src/main/java/com/leothosthoren/go4lunch/MainActivity.java
+++ app/src/main/java/com/leothosthoren/go4lunch/MainActivity.java
@@ -1,16 +1,103 @@
 package com.leothosthoren.go4lunch;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+
 import com.crashlytics.android.Crashlytics;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.leothosthoren.go4lunch.base.BaseActivity;
+
+import java.util.Arrays;
+
+import butterknife.BindView;
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    // 1 - Identifier for Sign-In Activity
+    private static final int RC_SIGN_IN = 123;
+    // 1 - Get Coordinator Layout
+    @BindView(R.id.main_activity_coordinator_layout)
+    CoordinatorLayout coordinatorLayout;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onResume() {
+        super.onResume();
         Fabric.with(this, new Crashlytics());
-        setContentView(R.layout.activity_main);
+        // Force application to crash
+//        Crashlytics.getInstance().crash();
+
+
+    }
+
+    @Override
+    public int getFragmentLayout() {
+        startSignInActivity();
+        return R.layout.activity_main;
+    }
+
+
+    // --------------------
+    // NAVIGATION
+    // --------------------
+
+    // 2 - Launch Sign-In Activity
+    private void startSignInActivity() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),//EMAIL
+                                        new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))//GOOGLE
+//                                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))//FACEBOOK
+//                                        new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()))//TWITTER
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.background_image)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 4 - Handle SignIn Activity response on activity result
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+    // --------------------
+    // UI
+    // --------------------
+
+    // 2 - Show Snack Bar with a message
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    // --------------------
+    // UTILS
+    // --------------------
+
+    // 3 - Method that handles response after SignIn Activity close
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/leothosthoren/go4lunch/base/BaseActivity.java
@@ -0,0 +1,37 @@
+package com.leothosthoren.go4lunch.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Sofiane M. alias Leothos Thoren on 04/05/2018
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    // --------------------
+    // LIFE CYCLE
+    // --------------------
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(getFragmentLayout());
+        ButterKnife.bind(this); //Configure Butterknife
+    }
+
+    public abstract int getFragmentLayout();
+
+    // --------------------
+    // UI
+    // --------------------
+
+    protected void configureToolbar(){
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+}
