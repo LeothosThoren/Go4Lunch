0d8f107fc674ef6523b9219dd1efb0354f9e0f20
Leothos_Thoren <soussou.med@live.fr>
2018-05-12 14:26:56 +0200

    Add Menu drawer

--- app/src/main/java/com/leothosthoren/go4lunch/controlers/Bottom_navigation.java
+++ app/src/main/java/com/leothosthoren/go4lunch/controlers/Bottom_navigation.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -18,12 +22,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.leothosthoren.go4lunch.R;
 
-
-import java.util.Objects;
-
 import butterknife.BindView;
 
-public class Bottom_navigation extends AppCompatActivity implements OnMapReadyCallback {
+public class Bottom_navigation extends AppCompatActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
 
 
     //    @BindView(R.id.navigation)
@@ -31,7 +32,11 @@ public class Bottom_navigation extends AppCompatActivity implements OnMapReadyCa
     GoogleMap mMap;
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
+
+    //Use bottom navigation view
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -58,7 +63,10 @@ public class Bottom_navigation extends AppCompatActivity implements OnMapReadyCa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
-        configureToolbar();
+        //Menu configuration
+        this.configureToolbar();
+        this.configureDrawerLayout();
+        this.configureNavigationView();
         //Bottom view configuration
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -66,6 +74,16 @@ public class Bottom_navigation extends AppCompatActivity implements OnMapReadyCa
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+    
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     @Override
@@ -93,13 +111,58 @@ public class Bottom_navigation extends AppCompatActivity implements OnMapReadyCa
         setSupportActionBar(mToolbar);
     }
 
+    //Handle menu drawer on back press button
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    public void onBackPressed() {
+        // 5 - Handle back click to close menu
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
+
+
+    //Handle the click on drawer menu
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        // 4 - Handle Navigation Item Click
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.nav_lunch:
+                break;
+            case R.id.nav_settings:
+                break;
+            case R.id.nav_logout:
+                break;
+            default:
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    // ---------------------
+    // CONFIGURATION
+    // ---------------------
+
+
+    // 2 - Configure Drawer Layout
+    private void configureDrawerLayout() {
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // 3 - Configure NavigationView
+    private void configureNavigationView() {
+        this.navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
 }
