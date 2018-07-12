eb1c0a9fdb62ccc15d1ace2d2b6779cdde6e2c43
leothos <soussou.med@live.fr>
2018-07-13 00:21:31 +0200

    Add interface for recyclerView

---
+++ app/src/main/java/com/leothosthoren/go4lunch/base/RecyclerViewBuilder.java
@@ -0,0 +1,10 @@
+package com.leothosthoren.go4lunch.base;
+
+public interface RecyclerViewBuilder {
+
+    void configureRecyclerView();
+
+    void configureSwipeRefreshLayout();
+
+    void updateUI();
+}
